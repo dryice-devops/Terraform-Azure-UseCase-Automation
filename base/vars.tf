cc1bc5d54f03e2241dcca310a577446ed8126970
Avinaw-HCL <81796245+Avinaw-HCL@users.noreply.github.com>
2021-04-02 18:18:37 +0530

    Update vars.tf

--- base/vars.tf
+++ base/vars.tf
@@ -1,9 +1,3 @@
-variable "rg_name" {
-  type        = string
-  default     = "Test"
-  description = "The name of the Resource Group where the Resource will be Created"
-}
-
 variable "vnet_name" {
   type        = string
   default     = "Test"
