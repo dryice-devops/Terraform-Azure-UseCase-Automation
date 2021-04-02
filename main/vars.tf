d0b09aaf37a2d72d6fd0958ef13eed21a77b10c5
Avinaw-HCL <81796245+Avinaw-HCL@users.noreply.github.com>
2021-04-02 18:23:41 +0530

    Update vars.tf

--- main/vars.tf
+++ main/vars.tf
@@ -1,16 +1,10 @@
-variable "rg_name" {
-  type        = string
-  default     = "Test"
-  description = "The name of the Resource Group where the Resource will be Created"
-}
-
 variable "vnet_name" {
   type        = string
   default     = "Test"
-  description = "The name of the Subnet to be used in VM Scale Set"
+  description = "The name of the Vnet to be used in VM Scale Set"
 }
 
-variable "Subnet_name" {
+variable "subnet_name" {
   type        = string
   default     = "Test"
   description = "The name of the Subnet to be used in VM Scale Set"
@@ -19,36 +13,36 @@ variable "Subnet_name" {
 variable "nsg_name" {
   type        = string
   default     = "Test"
-  description = "The name of the Network Sexcurity Group to be used to InBound and OutBound Traffic"
+  description = "The name of the Network Security Group to be used to InBound and OutBound Traffic"
 }
 
 variable "pip_name" {
   type        = string
   default     = "Test"
-  description = "The name of the Network Sexcurity Group to be used to InBound and OutBound Traffic"
+  description = "The name of the Public Ip for accessing VM"
 }
 
 variable "network_int_name" {
   type        = string
   default     = "Test"
-  description = "The name of the Network Sexcurity Group to be used to InBound and OutBound Traffic"
+  description = "The name of the Network interface"
 }
 
 variable "node_size" {
   type        = string
   default     = "Standard_DS1_v2"
-  description = "The name of the Network Sexcurity Group to be used to InBound and OutBound Traffic"
+  description = "The size of the Azure VM Node"
 }
 
 variable "username" {
   type        = string
-  default     = "Test"
-  description = "The name of the Network Sexcurity Group to be used to InBound and OutBound Traffic"
+  default     = "SU-user"
+  description = "The name of the user for VM Login"
 }
 
 variable "password" {
   type        = string
-  default     = "Test"
-  description = "The name of the Network Sexcurity Group to be used to InBound and OutBound Traffic"
+  default     = "#YLPRgg89"
+  description = "The password of the user for VM Login
 }
 
