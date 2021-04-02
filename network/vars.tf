84661d9a8cdcb088dcb9eaa25ad104fbbd4cd53d
Avinaw-HCL <81796245+Avinaw-HCL@users.noreply.github.com>
2021-04-02 18:25:44 +0530

    Update vars.tf

--- network/vars.tf
+++ network/vars.tf
@@ -1,13 +1,7 @@
-variable "rg_name" {
-  type        = string
-  default     = "Test"
-  description = "The name of the Resource Group where the Resource will be Created"
-}
-
 variable "vnet_name" {
   type        = string
   default     = "Test"
-  description = "The name of the Subnet to be used in VM Scale Set"
+  description = "The name of the VNet to be used in VM Scale Set"
 }
 
 variable "Subnet_name" {
